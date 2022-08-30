6cb868e545b75258d18de50c962ab5c2488c5327
trevor1107 <qnrudwns123@naver.com>
2022-08-30 19:11:49 +0900

    fix: 웹 애플리케이션 서버 에러 처리 수정
    오라클 클라우드 컴퓨팅에 배포시 에러로 인해 종료는 되는데
    오류에 대한 메시지를 확인하지 못해서 원인을 찾기 어려 웠습니다.
    원인은 nginx에서 80번 포트를 사용 중인데 이 node프로젝트 서버도 80번
    포트를 사용하면서 포트 충돌이 발생했습니다.
    그래서 Node프로젝트의 포트를 3001으로 바꾸고 nginx에 포트포워딩을 설정
    하였음

--- web_kpp.js
+++ web_kpp.js
@@ -54,7 +54,7 @@ function normalizePort(val) {
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    console.error(error);
   }
 
   var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
@@ -63,15 +63,12 @@ function onError(error) {
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
     case 'EADDRINUSE':
       console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
     default:
-      throw error;
+      console.error(error);
   }
+  process.exit(1);
 }
 
 /**
